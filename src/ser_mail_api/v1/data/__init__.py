3a3d3d9c5640e0a967dcfb08ce68364877f361d1
Ludvik Jerabek <ljerabek@proofpoint.com>
2025-01-27 10:57:03 -0500

    Refactor __init__.py relative paths and added missing __init__.py files.

---
+++ src/ser_mail_api/v1/data/__init__.py
@@ -0,0 +1,7 @@
+from .attachment import Attachment, FileAttachment, StreamAttachment, Disposition
+from .content import Content, ContentType
+from .mailuser import MailUser
+from .message import Message
+
+
+__all__ = ['Attachment','FileAttachment', 'StreamAttachment', 'Disposition', 'Content', 'ContentType', 'MailUser', 'Message']
