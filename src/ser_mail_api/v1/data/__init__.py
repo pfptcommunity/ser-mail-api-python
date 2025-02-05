6bda5509ddf326419b5fead8029c71327f7e0a87
Ludvik Jerabek <ljerabek@proofpoint.com>
2025-02-05 09:52:55 -0500

    Add MessageHeaders object
    Since SER API doesn't allow free form email headers but may add this in the future. MessageHeaders object places a strict constraint on the Header From.

--- src/ser_mail_api/v1/data/__init__.py
+++ src/ser_mail_api/v1/data/__init__.py
@@ -2,6 +2,7 @@ from .attachment import Attachment, Disposition
 from .content import Content, ContentType
 from .mailuser import MailUser
 from .message import Message
+from .message_headers import MessageHeaders
 from .result import Result
 
-__all__ = ['Attachment', 'Disposition', 'Content', 'ContentType', 'MailUser', 'Message', 'Result']
+__all__ = ['Attachment', 'Disposition', 'Content', 'ContentType', 'MailUser', 'Message', 'MessageHeaders', 'Result']
