2f4168583ba179d4df21755b7c79f4410157444f
Ludvik Jerabek <ljerabek@proofpoint.com>
2025-09-24 14:24:37 -0400

    Update region.py
    Added SER regions support

--- src/ser_mail_api/v1/common/region.py
+++ src/ser_mail_api/v1/common/region.py
@@ -8,5 +8,6 @@ from enum import Enum
 
 class Region(Enum):
     US = 'mail-us.ser.proofpoint.com'
+    CA = 'mail-ca.ser.proofpoint.com'
     EU = 'mail-eu.ser.proofpoint.com'
     AU = 'mail-aus.ser.proofpoint.com'
